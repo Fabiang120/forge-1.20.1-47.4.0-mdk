557cae5d4bedddf169befaa726cf9e8b1c80a1d8
Fabiang120 <117304307+Fabiang120@users.noreply.github.com>
2025-09-03 18:21:04 -0400

    ketocononite added properly, first item!!!!

--- src/main/java/net/fabian/tutorialmod/TutorialMod.java
+++ src/main/java/net/fabian/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.fabian.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.fabian.tutorialmod.Item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -25,7 +28,7 @@ public class TutorialMod
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
+        ModItems.register(modEventBus);
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -45,6 +48,9 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(ModItems.ketocononite);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
